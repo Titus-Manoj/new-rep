702da54bcfc5f572605c3129ac2442daf722ac22
Titus-Manoj <56962660+Titus-Manoj@users.noreply.github.com>
2019-10-24 19:35:18 +0530

    Add files via upload

---
+++ Binary Numbers.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+ // Check Examples
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+{
+    public static void main(String[] args)
+    {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int rem,count=0;
+            while(n>0){
+            	rem=n%2;
+            	n=n/2;
+            	if(rem==1)
+            		count++;
+            }
+            System.out.print(count);
+            	
+            	
+          
+         
+         
+            	
+            		
+            	
+            	
+          
+            	
+            
+       
+            
+        
+        
+ 	  
+ 	  
+ 	  
+   	
+ 	  
+ 	}
+ }
